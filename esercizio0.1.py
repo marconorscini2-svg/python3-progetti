ef8449e42f4e13d8dca7e9ba6b5da4d3bcf9faf6
Marco Norscini <marco.norscini2@gmail.it>
2026-09-25 18:24:09 +0200

    Pulito il progetto per ricominciare da capo

--- esercizio0.1.py
+++
@@ -1,3 +0,0 @@
-nome_utente = input("Ciao, come ti chiami?")
-citta = input("In quale città vivi?")
-print("Ciao " + nome_utente + ", so che vivi a " + citta + "!")
--- esercizio1.1.py
+++
@@ -1,9 +0,0 @@
-# Questa è una variabile che contiene del testo (Stringa)
-nome = "Mario"
-
-# Questa è una variabile che contiene un numero (Intero)
-eta = 25
-
-# Usiamo print() per vedere cosa c'è dentro le scatole
-print("Mario")
-print(25)
