9ff00f6e3aa203eb598a5f4710304f9e10171948
marconorscini2-svg <marco.norscini2@gmail.com>
2026-09-25 14:20:50 +0200

    Primo commit
    Primo commit del progetto

--- calcolo.py
+++
@@ -1,7 +0,0 @@
-# Contenuto calcolo.py
-a = 1000
-b = 3000
-c = a + b
-print(c)
-print(f"La somma di {a} e {b} è uguale a: {c}")
-
--- esempio50.py
+++
@@ -1,8 +0,0 @@
-# script di esempio
-import os
-
-def stampa_ciao():
-    print("Ciao, mondo!")
-
-# chiamata della funzione
-stampa_ciao()
