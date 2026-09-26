581501cb2fa69d20aca4af2bb5a5f237473e8c6f
marconorscini2-svg <marco.norscini2@gmail.com>
2026-09-26 19:45:35 +0200

    aggiornamento
    aggiornamento file

---
+++ script_librerie.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Tutte le librerie funzionano alla grande!")
+print("Versione di Pandas:", pd.__version__)
+print("Versione di NumPy:", np.__version__)
